4ddaf4d931fd594a30cf8ac15107378b83498a74
obusandy <obungasandrah@gmail.com>
2023-08-10 08:16:58 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints out output usings puts
+ * main - is the main function
+ * Using puts not printf
+ * Return: always (0)
+ */
+
+
+int main(void)
+{
+	("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

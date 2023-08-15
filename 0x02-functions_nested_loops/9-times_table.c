6b61c6b59ae84a7412215235b49556c1e9e4188e
obusandy <obungasandrah@gmail.com>
2023-08-15 09:33:59 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *times_table - prints 9 timetables
+ *Return: void
+ *
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (result < 10 && j != 0)
+				_putchar(' ');
+
+			if (result >= 10)
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+
+			if (j == 9)
+				_putchar('\n');
+		}
+	}
+}

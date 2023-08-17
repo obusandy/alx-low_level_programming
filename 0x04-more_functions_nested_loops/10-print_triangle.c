c224252d5f0696b6dd24c97c35fdbfcdc937a810
obusandy <obungasandrah@gmail.com>
2023-08-17 07:13:26 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,21 +11,25 @@ void print_triangle(int s)
 
 	if (s > 0)
 	{
-		for (i = 0; i < s; i++)
-		{
-		for (j = 0; j < s; j++)
-		{
-		if (j < s - (i + 1))
-		{
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar('#');
-		}
-		}
-		_putchar('\n');
-		}
+	for (i = 0; i < s; i++)
+	{
+	for (j = 0; j < s; j++)
+	{
+	if (j < s - (i + 1))
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('#');
+	}
+	}
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
 	}
 }
 

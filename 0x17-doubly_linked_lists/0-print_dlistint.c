8c208f8b00979ceaf72f84c9ca7bc5c9b89dd35c
obusandy <obungasandrah@gmail.com>
2023-11-09 07:32:21 -0800

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,12 @@
  */
 size_t print_dlistint(const dlistint_t *head_ptr)
 {
-    size_t nod_count;
+	size_t nod_count;
 
-    for (nod_count = 0; head_ptr != NULL; nod_count++)
-    {
-        printf("%d\n", head_ptr->n);
-        head_ptr = head_ptr->next;
-    }
-    return nod_count;
+	for (nod_count = 0; head_ptr != NULL; nod_count++)
+	{
+		printf("%d\n", head_ptr->n);
+		head_ptr = head_ptr->next;
+	}
+	return (nod_count);
 }
